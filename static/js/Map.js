119251639bad349a6cfe1adc19c135751e046c52
Adrian Agnic <adrian@tura.io>
2017-09-29 15:04:17 -0700

    [#151536419] skeleton map class

---
+++ static/js/Map.js
@@ -0,0 +1,19 @@
+class Map {
+
+  constructor(allDrivers, allRiders) {
+    this.allRiders = allRiders;
+    this.allDrivers = allDrivers;
+  }
+
+  createRider() {
+    console.log("works")
+  }
+
+  createDriver() {
+    console.log("works")
+  }
+
+  createTrip() {
+    console.log("works")
+  }
+}
