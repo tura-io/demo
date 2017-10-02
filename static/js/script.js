29cabb30a849503a0051ab876ab87c2d99e9d0ab
Adrian Agnic <adrian.agnic@hotmail.com>
2017-10-02 11:28:51 -0700

    Delete script.js
    remove old script file not used

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-import Map, Trip, Route, Person, Driver, Rider, Point
-
-var newMap = new Map(newDriver, newRider);
-var newTrip = new Trip(newRider, newDriver, newRoute, "PAID");
-var newRoute = new Route([0.0, 0.0], [1.1, 1.1]);
-var newPerson = new Person(0, "DRIVER", "TuraBot", newPoint);
-var newDriver = new Driver(true);
-var newRider = new Rider(true, newDriver);
-var newPoint = new Point([0.0, 0.0]);
