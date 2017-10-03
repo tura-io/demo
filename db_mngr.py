9024290c9dcacbb1ad0122295702620a871e8b50
Adrian Agnic <adrian@tura.io>
2017-10-03 14:07:35 -0700

    [#151528550] get DB location data to JS and initiate as a class

--- db_mngr.py
+++ db_mngr.py
@@ -1,12 +1,18 @@
 import sqlite3 as db
 
+test_locations = [ ['Tura.io', -122.67546, 45.502647], ['PSU', -122.65772, 45.52863], ['Salt N Straw', -122.698555, 45.528652], ['Powells City of Books', -122.681175, 45.52298], ['Portland Saturday Market', -122.67027, 45.523056] ]
+
 ################################USED FOR TESTING -TEMPORARY
 def create_w_data():
     conn = db.connect('demo.db')
     dbi = conn.cursor()
-    dbi.execute('''CREATE TABLE locations (id int, x real, y real)''') #sql-like commands, data types are limited so far
-    dbi.execute('''INSERT INTO locations VALUES (1, -122.67546, 45.502647)''')
-    conn.commit()
+    dbi.execute('''CREATE TABLE locations (
+    name text,
+    x real,
+    y real)''') #sql-like commands, data types are limited so far
+    for loc in test_locations:
+        dbi.execute('''INSERT INTO locations (name, x, y) VALUES (?, ?, ?)''', (loc[0], loc[1], loc[2]))
+        conn.commit()
     conn.close()
 
 def drop():
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,6 +4,11 @@ $(function() {
     url: '/db/read',
     dataType: 'json'
   }).done(function(result) {
-    alert(result);
+    let locList = [];
+    for (i = 0; i < result.length; i++) {
+      let newLoc = new Point(result[i][0], result[i][1], result[i][2]);
+      locList.push(newLoc);
+    };
+    console.log(locList);
   });
 });
