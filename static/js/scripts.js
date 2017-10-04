5f199877bac55c105889b39d8ab3a693f53e5349
Adrian Agnic <adrian@tura.io>
2017-10-04 12:43:59 -0700

    break down json response into more readable format

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,14 +1,24 @@
 $(function() {
   $.ajax({
-    url: '/db/read',
+    url: "https://api.mapbox.com/directions/v5/mapbox/driving/-122.67546,45.502647;-122.681175,45.52298?steps=true&access_token=pk.eyJ1IjoiY2hhcmxlc2VtcmljaCIsImEiOiJjajg2N3J6NnowczB4MndwbHQ5b3UwMnBrIn0.PWVhuPintFtBY8i9TqTW8w",
     dataType: 'json'
   }).done(function(result) {
-    let locList = [];
+    console.log(result);
+    console.log('');
 
-    for (i = 0; i < result.length; i++) {
-      let newLoc = new Point(result[i][0], result[i][1], result[i][2]);
-      locList.push(newLoc);
+    for (i = 0; i < result.waypoints.length; i++) {
+      console.log(result.waypoints[i].name);
+      console.log(result.waypoints[i].location);
     };
-    console.log(locList);
+    console.log('');
+
+    for (i = 0; i < result.routes.length; i++) {
+      for (j = 0; j < result.routes[i].legs.length; j++) {
+        for (k = 0; k < result.routes[i].legs[j].steps.length; k++) {
+          console.log(result.routes[i].legs[j].steps[k].maneuver.instruction);
+          console.log(result.routes[i].legs[j].steps[k].maneuver.location);
+        }
+      }
+    };
   });
 });
