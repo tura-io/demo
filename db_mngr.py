8e7d077e4d2842b23d0da6642815b67cc7ce4424
Charles Emrich <charles@tura.io>
2017-10-04 10:24:54 -0700

    [#151528846] reorganize index and combine script files, also begin building pin-drop

--- db_mngr.py
+++ db_mngr.py
@@ -1,6 +1,30 @@
 import sqlite3 as db
 
-test_locations = [ ['Tura.io', -122.67546, 45.502647], ['PSU', -122.65772, 45.52863], ['Salt N Straw', -122.698555, 45.528652], ['Powells City of Books', -122.681175, 45.52298], ['Portland Saturday Market', -122.67027, 45.523056] ]
+test_locations = [ ['Tura.io', -122.67546, 45.502647],
+                   ['PSU', -122.65772, 45.52863],
+                   ['Salt N Straw', -122.698555, 45.528652],
+                   ['Powells City of Books', -122.681175, 45.52298],
+                   ['Pioneer Courthouse Square', -122.679339, 45.518911],
+                   ['Ground Kontrol', -122.675932, 45.523959],
+                   ['The Nines', -122.677967, 45.519215],
+                   ['Andina', -122.684637, 45.526302],
+                   ['Momo\'s', -122.682527, 45.519687],
+                   ['Hotel Modera', -122.680713, 45.513792],
+                   ['Portland Marriott Downtown Waterfront', -122.675589, 45.512483],
+                   ['The Roxy', -122.682541, 45.522579],
+                   ['Voodoo Doughnuts', -122.673081, 45.522668],
+                   ['Lan Su Chinese Garden', -122.673289, 45.525384],
+                   ['Mary\'s Club', -122.677913, 45.522781],
+                   ['Providence Park', -122.691630, 45.521554],
+                   ['Higgins', -122.681996, 45.515576],
+                   ['Stag PDX', -122.677860, 45.525494],
+                   ['Hotel Lucia', -122.678446, 45.521078],
+                   ['Sushi Takahashi 2', -122.683509, 45.505513],
+                   ['Luc Lac Vietnamese Kitchen', -122.675428, 45.516866],
+                   ['Arlene Schnitzer Concert Hall', -122.681903, 45.517031],
+                   ['Roseland Theatre', -122.676196, 45.523215],
+                   ['The Armory', -122.681614, 45.524144],
+                   ['Portland Saturday Market', -122.67027, 45.523056] ]
 
 ################################USED FOR TESTING -TEMPORARY
 def create_w_data():
--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,48 @@
-//The accessToken below is a publicly provided one and doesn't need to be hidden.
-mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhcmxlc2VtcmljaCIsImEiOiJjajg2N3J6NnowczB4MndwbHQ5b3UwMnBrIn0.PWVhuPintFtBY8i9TqTW8w';
-var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/light-v9',
-center: [-122.6774833, 45.5206428],
-interactive: false,
-zoom: 15
-});
 
-//console.log(map);
+$(function() {
+  $.ajax({
+    url: '/db/read',
+    dataType: 'json'
+  }).done(function(result) {
+    let locList = [];
+
+    for (i = 0; i < result.length; i++) {
+      let newLoc = new Point(result[i][0], result[i][1], result[i][2]);
+      // TODO: Add locList to map object rather than leaving it global
+      locList.push(newLoc);
+    };
+    console.log(locList);
+  });
+
+  //The accessToken below is a publicly provided one and doesn't need to be hidden.
+  //TODO: Switch to our token and stash it in an appropriate way.
+  mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhcmxlc2VtcmljaCIsImEiOiJjajg2N3J6NnowczB4MndwbHQ5b3UwMnBrIn0.PWVhuPintFtBY8i9TqTW8w';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    center: [-122.6774833, 45.5206428],
+    interactive: false,
+    zoom: 15
+  });
+
+  map.on('load', function() {
+    map.addLayer({
+      "id": "points",
+      "type": "symbol",
+      "source": {
+        "type": "geojson",
+        "data": {
+          "type": "FeatureCollection",
+          "features": [locList]
+        }
+      },
+      "layout": {
+          "icon-image": "{icon}-15",
+          "text-field": "{title}",
+          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          "text-offset": [0, 0.6],
+          "text-anchor": "top"
+      }
+    });
+  });
+});
