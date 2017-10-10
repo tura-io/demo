b18c409e188ab1c7d1382745d1a79357a7f98335
Adrian Agnic <adrian@tura.io>
2017-10-09 17:00:46 -0700

    merge error in script.js, deleted duplicates

--- static/js/script.js
+++ static/js/script.js
@@ -14,16 +14,20 @@ $(function () { //on docu load
       console.log(response);
   });
 //=========================================================
+  $.ajax({
+    url: 'db/read',
+    dataType: 'json'
+  }).done(function(result) {
+    let locList = [];
 
-<<<<<<< HEAD
     for (i = 0; i < result.length; i++) {
       let newLoc = new Point(result[i][0], result[i][1], result[i][2]);
       // TODO: Add locList to map object rather than leaving it global
       locList.push(newLoc);
     };
+
     //The accessToken below is a publicly provided one and doesn't need to be hidden.
     //TODO: Switch to our token and stash it in an appropriate way.
-    mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhcmxlc2VtcmljaCIsImEiOiJjajg2N3J6NnowczB4MndwbHQ5b3UwMnBrIn0.PWVhuPintFtBY8i9TqTW8w';
     map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/light-v9',
@@ -36,7 +40,6 @@ $(function () { //on docu load
   })
   .then(function() {
     map.on('load', function() {
-      getRoute();
       map.addLayer({
         "id": "points",
         "type": "symbol",
@@ -55,53 +58,6 @@ $(function () { //on docu load
           "text-anchor": "top"
         }
       });
-=======
-  $.ajax({
-    url: 'db/read', //collect DB data
-    dataType: 'json'
-  }).done(function(data) {
-      let locList = [];
->>>>>>> 94d33317886a787029948fe8f50bcf2df632fcba
-
-      for (i = 0; i < data.length; i++) {
-        let newLoc = new Point(data[i][0], data[i][1], data[i][2]); //init data as objects
-
-        // TODO: add locList to Map object
-        locList.push(newLoc); //store locally
-      };
-
-
-      map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/light-v9',
-        center: [-122.6774833, 45.5206428],
-        //interactive: false,
-        zoom: 15
-      });
-      map.locations = locList;  //load map with locations
-      console.log(map.locations);
-
-  }).then(function() {
-      map.on('load', function() {
-        map.addLayer({  //add layer w/ location points
-          "id": "points",
-          "type": "symbol",
-          "source": {
-            "type": "geojson",
-            "data": {
-              "type": "FeatureCollection",
-              "features": map.locations
-            }
-          },
-          "layout": {
-            "icon-image": "{icon}-15",
-            "text-field": "{title}",
-            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-            "text-offset": [0, 0.6],
-            "text-anchor": "top"
-          }
-        });
-
 //================================================================ANIMATION
         var radius = 0.001
         function pointOnCircle(angle) {
