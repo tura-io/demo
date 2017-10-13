05c5fea8230a4b1da65ef5bcdb967653b1227b4d
Charles Emrich <charles@tura.io>
2017-10-13 15:49:53 -0700

    implement unique ID system for Trips

--- static/js/Trip.js
+++ static/js/Trip.js
@@ -1,6 +1,19 @@
 class Trip {
 
   constructor(thisRider, thisDriver, tripType) {
+    (function() {
+        var id_counter = 1;
+        Object.defineProperty(Object.prototype, "__uniqueId", {
+            writable: true
+        });
+        Object.defineProperty(Object.prototype, "uniqueId", {
+            get: function() {
+                if (this.__uniqueId == undefined)
+                    this.__uniqueId = id_counter++;
+                return this.__uniqueId;
+            }
+        });
+    }());
     this.Map = {};
     this.Rider = thisRider;
     this.Driver = thisDriver;
