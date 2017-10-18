9b7cd22759b8f167f9af74957f9cbfa573ad8911
Adrian Agnic <adrian@tura.io>
2017-10-18 11:38:16 -0700

    async issue resolved, new db manager class

--- dbm.py
+++ dbm.py
@@ -1,10 +1,20 @@
-import sqlite3 as db
+import sqlite3 as sql
 import json
 
-class DBM(object):
+class DBManager(object):
     def __init__(self, db):
-        self.conn = db.connect('demo.db')
+        self.conn = sql.connect(db, check_same_thread=False)
         self.dbi = self.conn.cursor()
 
-    def __end__(self):
+    def read_routes(self):
+        self.dbi.execute('''SELECT * FROM routes''')
+        result = self.dbi.fetchall()
+        return result
+
+    def select_query(self, arg):
+        self.dbi.execute(arg)
+        result = self.dbi.fetchall()
+        return result
+
+    def __del__(self):
         self.conn.close()
--- demo.py
+++ demo.py
@@ -1,17 +1,14 @@
 from flask import Flask, render_template
 import json
 import db_mngr, a_ok
+from dbm import DBManager
 
 app = Flask(__name__)
+DBM = DBManager('demo.db')
 
 @app.route('/')#methods=['GET','POST']
 def index():
     return render_template('index.html')
-
-@app.route('/db/create')
-def db_create():
-    db_mngr.create_w_data()
-    return render_template('db.html')
 
 @app.route('/db/read')
 def db_read():
@@ -20,20 +17,8 @@ def db_read():
 
 @app.route('/db/routes')
 def db_read_routes():
-    all_routes = db_mngr.read_routes()
+    all_routes = DBM.read_routes()
     return json.dumps(all_routes)
-
-@app.route('/db/routeinfo')
-def db_route_info():
-    res = db_mngr.read_route_info()
-    return json.dumps(res)
-
-@app.route('/db/drop')
-def db_drop():
-    db_mngr.drop()
-    db_mngr.drop_routes()
-    return render_template('db.html')
-
 
 if __name__ == '__main__':
     aok = False
