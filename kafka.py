88b4a02230e96deb588b8e9789ac406aea04fba3
Adrian Agnic <adrian@tura.io>
2017-11-02 10:24:36 -0700

    change producer to python class, fix partition bug

--- kafka.py
+++ kafka.py
@@ -2,26 +2,41 @@ import json
 from pykafka import KafkaClient
 import pykafka.utils.compression as Compression
 
-client = KafkaClient(hosts='127.0.0.1:9092') #NOTE: Connection to broker
+__version__ = '0.0.1'
+__author__ = 'Adrian Agnic <adrian@tura.io>'
 
-topic = client.topics[b'demo'] #NOTE: Topic selection (byte-str)
+class Producer():
+    """
+    Simple kafka producer to stream out generated json data
+    Connection properties hardcoded
+    """
+    def __init__(self):
+        self.client = KafkaClient(hosts='127.0.0.1:9092') #NOTE: Connection to broker
+        self.topic = self.client.topics[b'testdemo'] #NOTE: Topic selection (byte-str)
+        self.producer = self.topic.get_producer(delivery_reports=False)
+        self.count = 0
 
-# function to collect byte data and stream out
-def stream_out(data):
-    with topic.get_producer(delivery_reports=True) as producer: #asynchronous producer
-        count = 0
+    def stream_out(self, data):
+        """
+        Function that compresses source data, sets a partition key then streams out
+        """
         cdata = Compression.encode_snappy(data, xerial_compatible=True, xerial_blocksize=32768)
-        while True:
-            count += 1
-            bcount = str(count).encode()
-            producer.produce(cdata, partition_key=bcount)
-            if count % 5 ** 2 == 0:
-                while True:
-                    try:
-                        msg, exc = producer.get_delivery_report(block=False)
-                        if exc is not None:
-                            print('Delivery Failure for {}: {}'.format(msg.partition_key, repr(exc)))
-                        else:
-                            print('Success: {}'.format(msg.partition_key))
-                    except NameError, TypeError, ValueError:
-                        print('name, type, or value error')
+        bcount = str(self.count).encode()
+        self.count += 1
+        self.producer.produce(cdata, partition_key=bcount)
+        # if count % 5 ** 2 == 0:
+        #     while True:
+        #         try:
+        #             msg, exc = producer.get_delivery_report(block=False)
+        #             if exc is not None:
+        #                 print('Delivery Failure for {}: {}'.format(msg.partition_key, repr(exc)))
+        #             else:
+        #                 print('Success: {}'.format(msg.partition_key))
+        #         except ValueError:
+        #             print('value')
+        #         except TypeError:
+        #             print('type')
+        #         except NameError:
+        #             print('name')
+        #         except Queue.Empty:
+        #             print('empty queue')
--- static/js/Trip.js
+++ static/js/Trip.js
@@ -11,7 +11,7 @@ class Trip {
     this.Map = {};
     this.Driver = driver;
     this.Route = {};
-    this.arrayLimiter = 5001; //NOTE: size of packets sent to server
+    this.arrayLimiter = 10001; //NOTE: size of packets sent to server
     //This controls the rate at which the car moves by controlling animation refresh rate. 75ms default refresh speed moves the car in approximate realtime at 30mph. The current default, 0, allows the map to animate as quickly as it's able.
     this.Speed = 0;
     this.Color = (function() {
