05a5ecab8b338fc26d0d71936aa1bacfe72a428a
tura-david <david@tura.io>
2017-12-19 17:40:26 -0800

    Removed noise for now

--- static/js/Trip.js
+++ static/js/Trip.js
@@ -317,7 +317,7 @@ class Trip {
              myThis.Trigger = false
          }
 
-        myThis.emitNoisy(1, 5, 1);
+        myThis.emitNoisy(0, 0, 0);
         // Request the next frame of animation so long as destination has not
         // been reached.
         if (point.features[0].geometry.coordinates[0]
