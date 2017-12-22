bcac1807c140dd1d1d677d7e5a58a121f1fefe85
tura-david <david@tura.io>
2017-12-21 18:58:08 -0800

    Updated params that look good. TODO: fix spawning the same driver twice

--- static/js/Map.js
+++ static/js/Map.js
@@ -7,10 +7,10 @@ class MapBox extends mapboxgl.Map {
     this.trips = [];
     this.drivers = [];
     this.c = 0;
-    this.maxTrips = 1; //TODO: Make sure this works with Driver->Rider Trips the way we want.
+    this.maxTrips = 2; //TODO: Make sure this works with Driver->Rider Trips the way we want.
     this.tripSpawnInterval = 2000; //ms
     this.intervalId = 0;
-    this.initialDrivers = 1;
+    this.initialDrivers = 2;
     this.driverFirstNames = ['Parham', 'Justine', 'David', 'Molly', 'Cedar', 'Jack', 'Rachel', 'Adrian', 'Cheryl', 'Ricky'];
     this.driverLastNames = ['Parvizi', 'Wang', 'Nielsen', 'LeCompte', 'Mora', 'Emrich', 'Agnic', 'Smith', 'Wilson', 'Bobby'];
     this.eventDisplay = false;
